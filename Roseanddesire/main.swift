4b62740ce760b15d72ff5774cc0a58d71c9fa0de
Risakkuma <risakkuma@users.noreply.github.com>
2017-08-27 10:52:19 +0800

    Initial Commit

---
+++ Roseanddesire/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Roseanddesire
+//
+//  Created by Jane Risa Wang on 27/08/2017.
+//  Copyright © 2017 Jane Risa Wang. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
